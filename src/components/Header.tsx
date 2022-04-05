14bbbad2710e999f178797ae9f9c70d6ab30f7b7
FabioFiorita <fabiolfp@gmail.com>
2022-04-05 11:58:34 -0300

    Header dropdown menu to logout in mobile devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Button, Flex, Image, Spacer, Stack, Text } from "@chakra-ui/react";
+import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
+import { Button, Flex, IconButton, Image, Menu, MenuButton, MenuItem, MenuList, Spacer, Stack, Text } from "@chakra-ui/react";
 import { useFirebaseAuth } from "../contexts/FirebaseAuthContext";
 
 export function Header() {
@@ -8,14 +9,46 @@ export function Header() {
         <Flex
             as="nav"
             padding={5}
+            w="100%"
             backgroundColor="#1B1B1F"
             color="white"
         >
             <Image src="/mybills.png" alt="MyBills" mb={5} />
             <Spacer />
             <Flex
+                w="100%"
+                padding={5}
+                bg="#1B1B1F"
+                color="white"
+                display={["flex", "flex", "none"]}
+            >
+                <Menu>
+                    <MenuButton
+                        as={IconButton}
+                        aria-label='Options'
+                        icon={<HamburgerIcon />}
+                        variant='outline'
+                        _focus={ { bg: "#1B1B1F" } }
+                    />
+                    <MenuList
+                        bg="#1B1B1F"
+                    >
+                        <MenuItem
+                            color="white"
+                            _focus={ { bg: "#1B1B1F" } }
+                            icon={<CloseIcon />}
+                            command='⌘S'
+                            onClick={handleLogout}
+                        >
+                            Sair
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
+            </Flex>
+            <Flex
                 justifyContent="center"
                 flexDir="column"
+                display={["none", "none", "flex"]}
             >
                 <Flex
                     marginBottom={3}
