d582dc983728b9e01b99749edd5fd93f7859966d
avnerjose <avnerj.g.r@gmail.com>
2022-04-05 22:42:57 -0300

    Changing header to use popover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,80 +1,66 @@
-import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
-import { Button, Flex, IconButton, Image, Menu, MenuButton, MenuItem, MenuList, Spacer, Stack, Text } from "@chakra-ui/react";
+import { CloseIcon, HamburgerIcon, Icon } from "@chakra-ui/icons";
+import {
+  Avatar,
+  Button,
+  Flex,
+  Image,
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverFooter,
+  PopoverTrigger,
+  Spacer,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { MdLogout } from "react-icons/md";
 import { useFirebaseAuth } from "../contexts/FirebaseAuthContext";
 
 export function Header() {
-    const { user, handleLogout } = useFirebaseAuth();
+  const { user, handleLogout } = useFirebaseAuth();
 
-    return (
-        <Flex
-            as="nav"
-            padding={5}
-            w="100%"
-            backgroundColor="#1B1B1F"
-            color="white"
+  return (
+    <Flex as="nav" padding={5} w="100%" backgroundColor="#1B1B1F" color="white">
+      <Image src="/mybillsV2.svg" w={["200px","250px","350px"]} alt="MyBills" mb={5} />
+      <Spacer />
+      <Popover>
+        <PopoverTrigger>
+          <Avatar cursor="pointer" src={user?.avatar} />
+        </PopoverTrigger>
+        <PopoverContent
+          mr="16px"
+          borderRadius="10px"
+          _focus={{
+            outline: "none",
+          }}
         >
-            <Image src="/mybills.png" alt="MyBills" mb={5} />
-            <Spacer />
-            <Flex
-                w="100%"
-                padding={5}
-                bg="#1B1B1F"
-                color="white"
-                display={["flex", "flex", "none"]}
-            >
-                <Menu>
-                    <MenuButton
-                        as={IconButton}
-                        aria-label='Options'
-                        icon={<HamburgerIcon />}
-                        variant='outline'
-                        _focus={ { bg: "#1B1B1F" } }
-                    />
-                    <MenuList
-                        bg="#1B1B1F"
-                    >
-                        <MenuItem
-                            color="white"
-                            _focus={ { bg: "#1B1B1F" } }
-                            icon={<CloseIcon />}
-                            command='⌘S'
-                            onClick={handleLogout}
-                        >
-                            Sair
-                        </MenuItem>
-                    </MenuList>
-                </Menu>
-            </Flex>
-            <Flex
-                justifyContent="center"
-                flexDir="column"
-                display={["none", "none", "flex"]}
-            >
-                <Flex
-                    marginBottom={3}
-                >
-                    <Image src={user?.avatar} borderRadius="50%" h={16} />
-                    <Stack ml="1rem">
-                        <Text
-                            fontSize="lg"
-                            fontWeight="bold"
-                        >Olá, {user?.name}</Text>
-                        <Text
-                            fontSize="lg"
-                            fontWeight="bold"
-                        >{user?.email}</Text>
-                    </Stack>
-                </Flex>
-                <Button
-                    onClick={handleLogout}
-                    colorScheme="red"
-                    fontWeight="bold"
-                    fontSize="lg"
-                >Sair</Button>
-
-            </Flex>
-
-        </Flex>
-
-    );
+          <PopoverBody d="flex" flexDir="row" py="1rem">
+            <Image src={user?.avatar} borderRadius="50%" h={16} />
+            <Stack ml="1rem" justifyContent="center">
+              <Text fontSize="lg" fontWeight="bold" color="#1B1B1F">
+                Olá, {user?.name}
+              </Text>
+              <Text fontSize="lg" color="#1B1B1F">
+                {user?.email}
+              </Text>
+            </Stack>
+          </PopoverBody>
+          <PopoverFooter
+            cursor="pointer"
+            borderBottomRadius="10px"
+            bg="#DC1637"
+            onClick={handleLogout}
+            d="flex"
+            alignItems="center"
+            justifyContent="center"
+            fontWeight="bold"
+            gap="1rem"
+          >
+            Sair
+            <Icon as={MdLogout} />
+          </PopoverFooter>
+        </PopoverContent>
+      </Popover>
+    </Flex>
+  );
 }
