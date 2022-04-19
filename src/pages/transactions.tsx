67369cb471202716ac8c65cdac709dc1d574a880
FabioFiorita <fabiolfp@gmail.com>
2022-04-19 11:56:41 -0300

    wip transactions page: added transaction table

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, useDisclosure } from "@chakra-ui/react";
+import { Button, Container, Flex, useDisclosure, Text, Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -26,9 +26,37 @@ const Dashboard: NextPage = () => {
     <Flex flexDir="column">
       <Header />
       <NewTransactionModal isOpen={isOpen} onClose={onClose} />
-      <Flex justifyContent="flex-end" marginTop="10px" marginRight="100px">
-      <Button onClick={onOpen} bg="#DC1637">Open Modal</Button>
-      </Flex>
+      <Container marginTop="10px" maxW="2x1" px="8rem">
+        <Flex justifyContent="space-between">
+        <Text fontSize="36px" fontWeight="semibold" color="#414141" fontFamily="cursive">Transações</Text>
+        <Button onClick={onOpen} bg="#DC1637">Nova Transação</Button>
+        </Flex>
+       
+            <TableContainer w="100%">
+              <Table size="lg" w="100%">
+                <Thead>
+                  <Tr >
+                    <Th color="#969CB2">Categoria</Th>
+                    <Th>Data</Th>
+                    <Th>Título</Th>
+                    <Th isNumeric>Valor</Th>
+                    <Th>Tipo</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>Comida</Td>
+                    <Td>19/04/2022</Td>
+                    <Td>Pão de queijo</Td>
+                    <Td isNumeric>10,33</Td>
+                   
+                    <Td>Saída</Td>
+                  </Tr>
+                </Tbody>
+                
+              </Table>
+            </TableContainer>
+      </Container>
     </Flex>
   );
 };
