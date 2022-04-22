5ca9cdfb35f8de8730c4f5108e72fb54bea39c29
avnerjose <avnerj.g.r@gmail.com>
2022-04-21 22:22:57 -0300

    Creating app theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,12 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { FirebaseAuthProvider } from "../contexts/FirebaseAuthContext";
 import "../styles/main.css";
+import { theme } from "../themes/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <FirebaseAuthProvider>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </FirebaseAuthProvider>
