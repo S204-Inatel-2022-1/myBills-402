fc8905baf3e94298dcae9fd9889b416ea2966a4f
avnerjose <avnerj.g.r@gmail.com>
2022-04-21 22:27:53 -0300

    Modifing transactions page

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -1,4 +1,17 @@
-import { Button, Container, Flex, useDisclosure, Text, Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Flex,
+  useDisclosure,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Heading,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -23,39 +36,40 @@ const Dashboard: NextPage = () => {
   }
 
   return (
-    <Flex flexDir="column">
+    <Flex flexDir="column" bg="white.200" minH="100vh">
       <Header />
       <NewTransactionModal isOpen={isOpen} onClose={onClose} />
       <Container marginTop="10px" maxW="2x1" px="8rem">
-        <Flex justifyContent="space-between">
-        <Text fontSize="36px" fontWeight="semibold" color="#414141" fontFamily="cursive">Transações</Text>
-        <Button onClick={onOpen} bg="#DC1637">Nova Transação</Button>
+        <Flex justifyContent="space-between" mb="2rem">
+          <Heading fontSize="36px" fontWeight="semibold" color="#414141">
+            Transações
+          </Heading>
+          <Button onClick={onOpen} color="white" bg="red.500">
+            Nova Transação
+          </Button>
         </Flex>
-       
-            <TableContainer w="100%">
-              <Table size="lg" w="100%">
-                <Thead>
-                  <Tr >
-                    <Th color="#969CB2">Categoria</Th>
-                    <Th>Data</Th>
-                    <Th>Título</Th>
-                    <Th isNumeric>Valor</Th>
-                    <Th>Tipo</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  <Tr>
-                    <Td>Comida</Td>
-                    <Td>19/04/2022</Td>
-                    <Td>Pão de queijo</Td>
-                    <Td isNumeric>10,33</Td>
-                   
-                    <Td>Saída</Td>
-                  </Tr>
-                </Tbody>
-                
-              </Table>
-            </TableContainer>
+        <TableContainer w="100%">
+          <Table size="lg" w="100%" variant="customTable">
+            <Thead>
+              <Tr>
+                <Th>Categoria</Th>
+                <Th>Data</Th>
+                <Th>Título</Th>
+                <Th isNumeric>Valor</Th>
+                <Th>Tipo</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Comida</Td>
+                <Td>19/04/2022</Td>
+                <Td>Pão de queijo</Td>
+                <Td isNumeric>10,33</Td>
+                <Td>Saída</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
       </Container>
     </Flex>
   );
