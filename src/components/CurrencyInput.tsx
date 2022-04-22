a077b0bfdf027c7d3332a75a4501a1a3c3b865b6
avnerjose <avnerj.g.r@gmail.com>
2022-04-21 22:25:36 -0300

    Creating input to hanlde currency

--- src/components/CurrencyInput.tsx
+++ src/components/CurrencyInput.tsx
@@ -8,13 +8,14 @@ type CurrencyInputProps = {
 export function CurrencyInput({ data, setData }: CurrencyInputProps) {
   return (
     <LibCurrencyInput
+      max={10000000}
+      maxLength={12}
       className="currency-input"
       placeholder="Preço"
       allowNegativeValue={false}
       defaultValue={0}
       decimalsLimit={2}
       value={data}
-      
       intlConfig={{ locale: "pt-BR", currency: "BRL" }}
       onValueChange={(value) => setData(value ? value : "")}
     />
