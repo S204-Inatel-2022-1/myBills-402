9f93ebe7941eb7cbff60d9302fe355fedda7196a
FabioFiorita <fabiolfp@gmail.com>
2022-04-26 11:48:25 -0300

    Fetching transcations and showing in table

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -14,31 +14,64 @@ import {
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
 import { LoadingSplash } from "../components/LoadingSplash";
 import { NewTransactionModal } from "../components/NewTransactionModal";
 import { useFirebaseAuth } from "../contexts/FirebaseAuthContext";
+import { collection, query, where, getDocs, Timestamp, orderBy } from "firebase/firestore";
+import { db } from "../services/firebase";
+
+type Transaction = {
+  authorId: string;
+  name: string;
+  price: number;
+  isDeposit: boolean;
+  category: string;
+  createdAt: Timestamp;
+};
 
 const Dashboard: NextPage = () => {
   const { user, isAuthLoading } = useFirebaseAuth();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const router = useRouter();
-
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  
+  useEffect(() => {
+    if (user) {
+      getTransactions();
+    }
+  }, [user]);
+  
   useEffect(() => {
     if (!isAuthLoading && !user) {
       router.push("/");
     }
   }, [user, isAuthLoading]);
 
+  function handleClosingWithReloading() {
+    onClose();
+    getTransactions();
+  }
+
   if (isAuthLoading) {
     return <LoadingSplash />;
   }
 
+
+  async function getTransactions() {
+    const q = query(collection(db, "transactions"), orderBy("createdAt", "desc"), where("authorId", "==", user?.id));
+    const querySnapshot = await getDocs(q);
+    const transactionsList = querySnapshot.docs.map(doc => doc.data() as Transaction);
+    setTransactions(transactionsList);
+  }
+
+
+
   return (
     <Flex flexDir="column" bg="white.200" minH="100vh">
       <Header />
-      <NewTransactionModal isOpen={isOpen} onClose={onClose} />
+      <NewTransactionModal isOpen={isOpen} onClose={handleClosingWithReloading} />
       <Container marginTop="10px" maxW="2x1" px="8rem">
         <Flex justifyContent="space-between" mb="2rem">
           <Heading fontSize="36px" fontWeight="semibold" color="#414141">
@@ -59,14 +92,20 @@ const Dashboard: NextPage = () => {
                 <Th>Tipo</Th>
               </Tr>
             </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Comida</Td>
-                <Td>19/04/2022</Td>
-                <Td>Pão de queijo</Td>
-                <Td isNumeric>10,33</Td>
-                <Td>Saída</Td>
-              </Tr>
+            <Tbody gap="0.5rem">
+                {transactions.length > 0 && (
+                  <>
+              {transactions?.map((transaction) => (
+                <Tr key={transaction.name} bgColor="white" borderRadius="10" mb="0.5rem">
+                  <Td>{transaction.category}</Td>
+                  <Td>{transaction.createdAt.toDate().toLocaleDateString()}</Td>
+                  <Td>{transaction.name}</Td>
+                  <Td isNumeric>{transaction.price}</Td>
+                  <Td>{transaction.isDeposit ? "Depósito" : "Saída"}</Td>
+                </Tr>
+              ))}
+                </>
+              )}
             </Tbody>
           </Table>
         </TableContainer>
